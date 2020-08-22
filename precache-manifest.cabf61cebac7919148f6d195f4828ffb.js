0edb027ae4a0d942e5802086557c98e71ad97eb5
Iris Mazzuca <irismazzuca@gmail.com>
2020-08-22 15:23:19 -0300

    Updates

---
+++ precache-manifest.cabf61cebac7919148f6d195f4828ffb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a7ce1f4337f219270a8a342c216d98",
+    "url": "/movies-app-react/index.html"
+  },
+  {
+    "revision": "d0211108b004f0668be3",
+    "url": "/movies-app-react/static/css/main.cde18d24.chunk.css"
+  },
+  {
+    "revision": "bd1481c91c95c2751d2c",
+    "url": "/movies-app-react/static/js/2.041ad7d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-app-react/static/js/2.041ad7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0211108b004f0668be3",
+    "url": "/movies-app-react/static/js/main.b28371f6.chunk.js"
+  },
+  {
+    "revision": "fe4dc21c3a61f03348b4",
+    "url": "/movies-app-react/static/js/runtime-main.f01c30ac.js"
+  }
+]);
